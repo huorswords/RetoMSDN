ab8185ae00dbb00dbecb9068ead6f726ac1d77fa
HuorSwords <huorswords@gmail.com>
2014-09-05 14:47:28 +0200

    Solución al reto 1 de MSDN
    Se han sobrecargado los métodos 'GetHashCode()' y 'Equal(object)' de la clase 'Persona' para permitir la comparación explícita de objetos por Nombre y Edad.
    
    Además, para la ordenación, se ha creado una clase derivada de 'IComparer<Persona>' que permite hacer la ordenación personalizada de listas sin necesidad de alterar la clase 'Persona'.

---
+++ Reto1ClassLibrary/Persona.cs
@@ -0,0 +1,41 @@
+﻿namespace Reto1ClassLibrary
+{
+    using System.Collections.Generic;
+
+    public class Persona
+    {
+        public string Nombre { get; set; }
+
+        public int Edad { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            return this.Edad.Equals(((Persona)obj).Edad)
+                && this.Nombre.Equals(((Persona)obj).Nombre);
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Nombre.GetHashCode() * 21 + this.Edad.GetHashCode() * 17;
+        }
+
+        internal sealed class PersonaComparer : IComparer<Persona>
+        {
+            public int Compare(Persona x, Persona y)
+            {
+                int result = y.Edad.CompareTo(x.Edad);
+                if (result == 0)
+                {
+                    result = x.Nombre.CompareTo(y.Nombre);
+                }
+
+                return result;
+            }
+        }
+    }
+}
---
+++ Reto1ClassLibrary/Reto1.cs
@@ -0,0 +1,17 @@
+﻿namespace Reto1ClassLibrary
+{
+    using System.Collections.Generic;
+
+    public class Reto1
+    {
+        public static List<Persona> OrdenarLista(List<Persona> lista)
+        {
+            if (null != lista)
+            {
+                lista.Sort(new Persona.PersonaComparer());
+            }
+
+            return lista;
+        }
+    }
+}
