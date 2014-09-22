1b246cf007f1fb1bb68cca75f99c5407d8929a79
Angel.G <angel.g-santos@itece.es>
2014-09-22 09:04:09 +0200

    Solución para el "Reto 2" de Microsoft Developer Spain.
    La implementación de la clase Reto2 consiste en la sobrecarga del registro
    y del desregistro del evento, de modo que en vez de registrar directamente
    el evento al delegado, se introduce en una variable de tipo 'SortedList'.
    
    De ese modo, y una vez se lanza el evento, el proceso registra EN ORDEN
    las solicitudes de registro de cada botón, de modo que cuando se lanza el
    evento se muestran los eventos en orden.

---
+++ Reto2/Reto2WPFApp/Reto2ClassLibrary/Reto2.cs
@@ -0,0 +1,54 @@
+﻿namespace Reto2ClassLibrary
+{
+    using System;
+    using System.Collections;
+
+    public class Reto2 : IReto2
+    {
+        private SortedList events = new SortedList();
+        private event EventHandler MyPrivateEventFired;
+
+        public event System.EventHandler EventFired
+        {
+            add
+            {
+                events.Add(((Item)value.Target).Index, value);
+            }
+            remove
+            {
+                events.Remove(((Item)value.Target).Index);
+            }
+        }
+
+        public void FireEvent()
+        {
+            this.PerformActionOnEvents(RegisterEvent);
+
+            if (MyPrivateEventFired != null)
+            {
+                MyPrivateEventFired(this, null);
+            }
+
+            this.PerformActionOnEvents(UnregisterEvent);
+            this.events.Clear();
+        }
+
+        private void PerformActionOnEvents(Action<object> action)
+        {
+            for (int i = 0; i < events.Count; i++)
+            {
+                action((EventHandler)events.GetByIndex(i));
+            }
+        }
+
+        private void RegisterEvent(object handler)
+        {
+            MyPrivateEventFired += (EventHandler)handler;
+        }
+
+        private void UnregisterEvent(object handler)
+        {
+            MyPrivateEventFired -= (EventHandler)handler;
+        }
+    }
+}
