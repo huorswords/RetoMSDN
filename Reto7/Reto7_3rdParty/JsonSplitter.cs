d4ac0c3ddf78c8dd654af4c149cdccf11c5f761f
Angel.G <angel.g-santos@itece.es>
2014-12-01 08:34:26 +0100

    Includes solution using Newtonsoft.Json library (JSON.NET)

---
+++ Reto7/Reto7_3rdParty/JsonSplitter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reto7
+{
+    using Newtonsoft.Json;
+
+    public class JsonSplitter
+    {
+        public static JsonResult SplitShowsByGenre(string jsonString, string genre)
+        {
+            Show[] collection = JsonConvert.DeserializeObject<Show[]>(jsonString);
+
+            return new JsonResult
+            {
+                Item1 = JsonConvert.SerializeObject(collection.Where(x => x.genres.Contains(genre))),
+                Item2 = JsonConvert.SerializeObject(collection.Where(x => !x.genres.Contains(genre))),
+            };            
+        }
+    }
+}
