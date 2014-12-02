de74ae79af452e525eefda5d584a710150c293c3
Angel.G <angel.g-santos@outlook.com>
2014-12-02 13:31:34 +0100

    Removes unused using statements.

--- Reto7/Reto7_3rdParty/JsonSplitter.cs
+++ Reto7/Reto7_3rdParty/JsonSplitter.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Reto7
+﻿namespace Reto7
 {
     using Newtonsoft.Json;
+    using System.Linq;
 
     public class JsonSplitter
     {
--- Reto7/Reto7_3rdParty/Show.cs
+++ Reto7/Reto7_3rdParty/Show.cs
@@ -1,10 +1,6 @@
 ﻿namespace Reto7
 {
-    using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     internal class Show
     {
