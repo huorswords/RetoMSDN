0652508a186a3c66f57932cbe3a6282987c6daa5
Angel.G <angel.g-santos@outlook.com>
2014-12-03 17:49:02 +0100

    Adds new JoinSplitter project, this time using ServiceStack.Text library.

---
+++ Reto7/Reto7.ServiceStack/JsonSplitter.cs
@@ -0,0 +1,37 @@
+﻿namespace Reto7
+{
+    using ServiceStack.Text;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+
+    public static class JsonSplitter
+    {
+        public static Tuple<string, string> SplitShowsByGenre(string json, string genre)
+        {
+            ICollection<Show> matches = new Collection<Show>();
+            ICollection<Show> others = new Collection<Show>();
+
+            JsConfig.IncludeNullValues = true;
+
+            Show[] collection = JsonSerializer
+                .DeserializeFromString<Show[]>(json);
+
+            foreach (var item in collection)
+            {
+                if (item.genres.Contains(genre))
+                {
+                    matches.Add(item);
+                    continue;
+                }
+
+                others.Add(item);
+            }
+
+            return new Tuple<string, string>(
+                    JsonSerializer.SerializeToString(matches),
+                    JsonSerializer.SerializeToString(others));
+        }
+    }
+}
