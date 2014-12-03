46a0f8b2987c22d963fd417aa08d789a5cad658d
Angel.G <angel.g-santos@outlook.com>
2014-12-03 17:48:16 +0100

    Improves fastJson use and increase performance a bit.
    Also, the code length is reduced.

--- Reto7/Reto7.fastJson/JsonSplitter.cs
+++ Reto7/Reto7.fastJson/JsonSplitter.cs
@@ -1,38 +1,32 @@
 ﻿namespace Reto7
 {
+    using fastJSON;
+    using System;
+    using System.Collections;
     using System.Collections.Generic;
-    using System.Diagnostics;
-    using System.Linq;
 
     public static class JsonSplitter
     {
-        public static JsonResult SplitShowsByGenre(string json, string genre)
+        public static Tuple<string, string> SplitShowsByGenre(string json, string genre)
         {
-            var result = new JsonResult();
-            var genreList = new List<object>();
-            var otherList = new List<object>();
-            
-            object[] collection = (object[])fastJSON.JSON.ToObject(json);
-            foreach (Dictionary<string, object> item in collection)
+            var genreList = new ArrayList();
+            var otherList = new ArrayList();
+
+            foreach (Dictionary<string, object> item in (object[])JSON.ToObject(json))
             {
-                var genres = ((List<object>)item["genres"]);
-                if (genres.Contains(genre))
+                if (((List<object>)item["genres"]).Contains(genre))
                 {
                     genreList.Add(item);
+                    continue;
                 }
-                else
-                {
-                    otherList.Add(item);
-                }
+
+                otherList.Add(item);
             }
 
-            fastJSON.JSONParameters param = new fastJSON.JSONParameters();
-            param.UseEscapedUnicode = false;
-
-            result.Item1 = fastJSON.JSON.ToJSON(genreList, param);
-            result.Item2 = fastJSON.JSON.ToJSON(otherList, param);
-
-            return result;
+            JSONParameters param = new JSONParameters { UseEscapedUnicode = false };
+            return new Tuple<string, string>(
+                JSON.ToJSON(genreList, param), 
+                JSON.ToJSON(otherList, param));
         }
     }
 }
